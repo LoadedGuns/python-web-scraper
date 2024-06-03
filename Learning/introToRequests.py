df6d176d021f7330bb5b97b07d792b8de8fa328e
LoadedGuns <jsweetsir18@icloud.com>
2024-06-03 14:35:29 -0400

    close value scraped

--- Learning/introToRequests.py
+++ Learning/introToRequests.py
@@ -6,16 +6,22 @@ import requests
 
 url = "https://finance.yahoo.com/quote/AAPL/"
 
-prop = "Previous Close"
-
 response = requests.get(url)
 
 print(response)
 print("HTML Status Code is " + str(response.status_code))
-text = response.text
 
-ind = text.index("Previous Close")
+if response.status_code == 200:
+    text = response.text
 
-redText = text[ind:].split("</span>")[1]
-val = redText.split(">")[-1]
-print(val)
+    ind = text.find("Previous Close")
+    
+    if ind != -1:
+        start_index = text.find('value="', ind) + len('value="')
+        end_index = text.find('"', start_index)
+        val = text[start_index:end_index]
+        print("Previous Close value is " + val)
+    else:
+        print("Property not found in the HTML content.")
+else:
+    print("Failed to retrieve the webpage.")
